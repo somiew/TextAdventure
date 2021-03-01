c392be303c5cf21d072575fe3fbfb4ade902d193
somiew <somiew@hotmail.com>
2021-03-01 12:34:21 +0100

    added inventory and gave player two free drinks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # python 3.7
+from random import randint
 
 class person:
     def __init__(self, name, age):
@@ -17,13 +18,13 @@ class liquid:
 
 
 if __name__ == '__main__':
-    zingo = liquid('Zingo', 'Yellow', True, False, False, 0)
-    coke = liquid('Coca Cola', 'Brown', True, False, False, 0)
-    norrlands = liquid('Norrlands Guld', 'Yellow', True, False, False, 18)
-    milk = liquid('Milk', 'White', False, True, False, 0)
-    oboy = liquid("O'boy", 'Brown', False, False, False, 0)
-    coffe = liquid('Coffee', 'Brown', False, False, False, 0)
-    posion = liquid('Poison', 'Green', False, False, True, 18)
+    drink1 = liquid('Zingo', 'Yellow', True, False, False, 0)
+    drink2 = liquid('Coca Cola', 'Brown', True, False, False, 0)
+    drink3 = liquid('Norrlands Guld', 'Yellow', True, False, False, 18)
+    drink4 = liquid('Milk', 'White', False, True, False, 0)
+    drink5 = liquid("O'boy", 'Brown', False, False, False, 0)
+    drink6 = liquid('Coffee', 'Brown', False, False, False, 0)
+    drink7 = liquid('Poison', 'Green', False, False, True, 18)
 
     print('Welcome to Sams text based adventure game!')
     print("What's your name?")
@@ -33,7 +34,22 @@ if __name__ == '__main__':
     player = person(inputName, inputAge)
     print("Hello " + player.name + ". Let's begin.")
 
-    # while player.tries > 0:
-    #     action = input()
-    #     print('Your action was: ' + action)
-    #     player.tries -= 1
+    inventory = []
+
+    for i in range(0, 2):
+        drinkNr = randint(1, 7)
+        drink = 'drink'
+        drink += str(drinkNr)
+        inventory.append(eval(drink).name)
+
+    print("As a little gift, you got two drinks in your inventory.")
+    print("To check your inventory, try to use the inventory action.")
+
+    while player.tries > 0:
+        action = input('Use an action: ')
+        if action == 'inventory':
+            print('\n--INVENTORY--')
+            for i in inventory:
+                print(i)
+            print('-------------')
+        player.tries -= 1
