24ae849c88d2ed0059bf1e94dfcb8bbb9736a946
somiew <somiew@hotmail.com>
2021-03-02 21:20:01 +0100

    moved objects to it's own script, drinks are now drinkable, no effects though

--- main.py
+++ main.py
@@ -1,55 +1,46 @@
-# python 3.7
+# python 3.8.7
+import objects
 from random import randint
 
-class person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.tries = 5
+drink1 = objects.liquid('Zingo', 'Yellow', True, False, False, 0)
+drink2 = objects.liquid('Coca Cola', 'Brown', True, False, False, 0)
+drink3 = objects.liquid('Norrlands Guld', 'Yellow', True, False, False, 18)
+drink4 = objects.liquid('Milk', 'White', False, True, False, 0)
+drink5 = objects.liquid("O'boy", 'Brown', False, False, False, 0)
+drink6 = objects.liquid('Coffee', 'Brown', False, False, False, 0)
+drink7 = objects.liquid('Poison', 'Green', False, False, True, 18)
 
-class liquid:
-    def __init__(self, nameStr, colourStr, sparklyBool, healthyBool, lethalBool, ageRestrictionInt):
-        self.name = nameStr
-        self.colour = colourStr
-        self.sparkly = sparklyBool
-        self.healthy = healthyBool
-        self.lethal = lethalBool
-        self.ageRestriction = ageRestrictionInt
+print('Welcome to Sams text based adventure game!')
+print("What's your name?")
+inputName = input()
+print("How old are you?")
+inputAge = input()
+player = objects.person(inputName, inputAge)
+print("Hello " + player.name + ". Let's begin.")
 
+inventory = []
 
-if __name__ == '__main__':
-    drink1 = liquid('Zingo', 'Yellow', True, False, False, 0)
-    drink2 = liquid('Coca Cola', 'Brown', True, False, False, 0)
-    drink3 = liquid('Norrlands Guld', 'Yellow', True, False, False, 18)
-    drink4 = liquid('Milk', 'White', False, True, False, 0)
-    drink5 = liquid("O'boy", 'Brown', False, False, False, 0)
-    drink6 = liquid('Coffee', 'Brown', False, False, False, 0)
-    drink7 = liquid('Poison', 'Green', False, False, True, 18)
+for i in range(0, 2):
+    drinkNr = randint(1, 7)
+    drink = 'drink'
+    drink += str(drinkNr)
+    inventory.append(drink)
 
-    print('Welcome to Sams text based adventure game!')
-    print("What's your name?")
-    inputName = input()
-    print("How old are you?")
-    inputAge = input()
-    player = person(inputName, inputAge)
-    print("Hello " + player.name + ". Let's begin.")
+print("As a little gift, you got two drinks in your inventory.")
+print("To check your inventory, try to use the inventory action.")
 
-    inventory = []
+while player.tries > 0:
+    action = input('Use an action: ')
+    if action == 'inventory':
+        print('\n--INVENTORY--')
+        for i in inventory:
+            print(eval(i).name)
+        print('-------------')
 
-    for i in range(0, 2):
-        drinkNr = randint(1, 7)
-        drink = 'drink'
-        drink += str(drinkNr)
-        inventory.append(eval(drink).name)
+    for each in inventory:
+        if action.lower() == 'drink ' + str(eval(each).name).lower():
+            print('You drank '+ str(eval(each).name))
+            inventory.remove(each)
+            break
 
-    print("As a little gift, you got two drinks in your inventory.")
-    print("To check your inventory, try to use the inventory action.")
-
-    while player.tries > 0:
-        action = input('Use an action: ')
-        if action == 'inventory':
-            print('\n--INVENTORY--')
-            for i in inventory:
-                print(i)
-            print('-------------')
-        player.tries -= 1
+    player.tries -= 1
---
+++ objects.py
@@ -0,0 +1,15 @@
+# python 3.8.7
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.tries = 5
+
+class liquid:
+    def __init__(self, nameStr, colourStr, sparklyBool, healthyBool, lethalBool, ageRestrictionInt):
+        self.name = nameStr
+        self.colour = colourStr
+        self.sparkly = sparklyBool
+        self.healthy = healthyBool
+        self.lethal = lethalBool
+        self.ageRestriction = ageRestrictionInt
